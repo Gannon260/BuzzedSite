ad462dd837084ebeffb12889f5f27cf1d0d44f2a
alekyap1 <32679272+alekyap1@users.noreply.github.com>
2017-10-14 16:09:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,70 +8,70 @@ var even=0;
 //callback is a function
 function deleteContent(callback) {
 
-    var intervalId = setInterval(function() {
-        if (fancyText.innerHTML.length == 0) {
-            clearInterval(intervalId);
+  var intervalId = setInterval(function() {
+    if (fancyText.innerHTML.length == 0) {
+      clearInterval(intervalId);
 
-            if (callback) {
-                setTimeout(callback, callbackPause);
-            }
-        }
+      if (callback) {
+        setTimeout(callback, callbackPause);
+      }
+    }
 
-        fancyText.innerHTML = fancyText.innerHTML.substring(0, fancyText.innerHTML.length - 1);
-    }, intervalTime);
+    fancyText.innerHTML = fancyText.innerHTML.substring(0, fancyText.innerHTML.length - 1);
+  }, intervalTime);
 
 }
 
 function addContent(contentToAdd, callback) {
-    var currentIndex = 0;
+  var currentIndex = 0;
 
-    var intervalId = setInterval(function() {
-        if (currentIndex == contentToAdd.length) {
-            clearInterval(intervalId);
+  var intervalId = setInterval(function() {
+    if (currentIndex == contentToAdd.length) {
+      clearInterval(intervalId);
 
-            if (callback) {
-                setTimeout(callback, callbackPause);
-            }
-        }
+      if (callback) {
+        setTimeout(callback, callbackPause);
+      }
+    }
 
-        fancyText.innerHTML = contentToAdd.substring(0, currentIndex);
-        currentIndex++;
-    }, intervalTime);
+    fancyText.innerHTML = contentToAdd.substring(0, currentIndex);
+    currentIndex++;
+  }, intervalTime);
 }
 
 setTimeout(function() {
-    deleteContent(function() {
-        addContent("a Comp Sci major", function() {
-            deleteContent(function() {
-                addContent("in love with puppies", function() {
-                    deleteContent(function() {
-                        addContent("Alekya");
-                    })
-                });
-            });
+  deleteContent(function() {
+    addContent("a Comp Sci major", function() {
+      deleteContent(function() {
+        addContent("in love with puppies", function() {
+          deleteContent(function() {
+            addContent("Alekya");
+          })
         });
+      });
     });
+  });
 }, initialPause);
 
 
 function checkboxes()
-        {
-         var inputElems = document.getElementsByTagName("input"),
-          count = 0;
+{
+  var inputElems = document.getElementsByTagName("input"),
+  count = 0;
 
 
-          for (var i=0; i<inputElems.length; i++) {
-            if(i%2 == 0 && inputElems[i].type == "checkbox" && inputElems[i].checked == true)
-            {
-              odd++;
+  for (var i=0; i<inputElems.length; i++) {
+    if(i%2 == 0 && inputElems[i].type == "checkbox" && inputElems[i].checked == true)
+    {
+      odd++;
 
-            }
-            if(i%2 == 1 && inputElems[i].type == "checkbox" && inputElems[i].checked == true)
-            {
-              even++;
+    }
+    if(i%2 == 1 && inputElems[i].type == "checkbox" && inputElems[i].checked == true)
+    {
+      even++;
 
-            }
+    }
 
-          }
-            document.getElementById("printed").innerHTML = odd+even;
-       }
+  }
+  document.getElementById("printed").innerHTML = odd+even;
+}
