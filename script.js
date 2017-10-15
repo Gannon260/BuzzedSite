db0f3ef7e4ea126c685ca97ea5257d9d09bf0f4a
Unknown <Peterslee40@yahoo.com>
2017-10-14 21:00:37 -0400

    Timestamp add

--- script.js
+++ script.js
@@ -2,69 +2,23 @@ var fancyText = document.getElementById('fancy');
 var intervalTime = 150;
 var initialPause = 1000;
 var callbackPause = 500;
-var odd=0;
-var even=0;
 
-//callback is a function
-function deleteContent(callback) {
-
-  var intervalId = setInterval(function() {
-    if (fancyText.innerHTML.length == 0) {
-      clearInterval(intervalId);
-
-      if (callback) {
-        setTimeout(callback, callbackPause);
-      }
-    }
-
-    fancyText.innerHTML = fancyText.innerHTML.substring(0, fancyText.innerHTML.length - 1);
-  }, intervalTime);
-
-}
-
-function addContent(contentToAdd, callback) {
-  var currentIndex = 0;
-
-  var intervalId = setInterval(function() {
-    if (currentIndex == contentToAdd.length) {
-      clearInterval(intervalId);
-
-      if (callback) {
-        setTimeout(callback, callbackPause);
-      }
-    }
-
-    fancyText.innerHTML = contentToAdd.substring(0, currentIndex);
-    currentIndex++;
-  }, intervalTime);
-}
-
-setTimeout(function() {
-  deleteContent(function() {
-    addContent("a Comp Sci major", function() {
-      deleteContent(function() {
-        addContent("in love with puppies", function() {
-          deleteContent(function() {
-            addContent("Alekya");
-          })
-        });
-      });
-    });
-  });
-}, initialPause);
-
-function writeUserScore(social, academics) {
+function writeUserScore(social, academics, Usertime) {
   firebase.database().ref('user-scores').push({
     social: social,
-    academics: academics
+    academics: academics,
+    Usertime: Usertime
   });
 }
 
-function checkboxes()
+function submitQuiz()
 {
   var inputElems = document.getElementsByTagName("input"),
   count = 0;
 
+
+  var odd=0;
+  var even=0;
 
   for (var i=0; i<inputElems.length; i++) {
     if(i%2 == 0 && inputElems[i].type == "checkbox" && inputElems[i].checked == true)
@@ -79,6 +33,9 @@ function checkboxes()
     }
 
   }
+
+  var Usertime = new Date().getTime()
+
   document.getElementById("printed").innerHTML = odd+even;
-  writeUserScore(odd, even);
+  writeUserScore(odd, even, Usertime);
 }
